145b2264f0cc97e6abd5a811957d387e2d941af4
Shajonblue <63549180+Shajon12@users.noreply.github.com>
2021-10-31 23:36:08 +0600

    My second project

---
+++ js/type.js
@@ -0,0 +1,49 @@
+var typed5 = new Typed(".typed", {
+  strings: [
+    "Web Design Services",
+    "Best UI/UX Design",
+    "Digital Marketing",
+    "SEO  Expert",
+  ],
+
+  typeSpeed: 50,
+  backSpeed: 10,
+  loop: true,
+});
+// $(document).ready(function () {
+//   $("html").bind("cut copy", function (e) {
+//     e.preventDefault();
+//     window.alert("You do not copy any text");
+//   });
+// });
+// window.addEventListener("load", () => {
+//   var ele = document.querySelector(".pro_html");
+//   var width = 1;
+//   var Time = setInterval(myfunction, 20);
+
+//   function myfunction() {
+//     if (width >= 95) {
+//       clearInterval(Time);
+//     } else {
+//       width++;
+//       ele.style.width = width + "%";
+//       document.querySelector("#html").innerHTML = width + "%";
+//     }
+//   }
+// });
+
+// window.addEventListener("load", () => {
+//   var ele = document.querySelector(".pro_html");
+//   var width = 1;
+//   var Time = setInterval(myfunction, 20);
+
+//   function myfunction() {
+//     if (width >= 95) {
+//       clearInterval(Time);
+//     } else {
+//       width++;
+//       ele.style.width = width + "%";
+//       document.querySelector("#html").innerHTML = width + "%";
+//     }
+//   }
+// });
